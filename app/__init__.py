1ed742098299b570d25fb9e3081bb3c62563ed78
Onichi <xokamoka07@gmail.com>
2023-05-16 22:52:05 +0300

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    return app
+
+app = create_app()
+from app import routes, models
